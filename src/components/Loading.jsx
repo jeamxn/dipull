6bd1a0dc731b5590bea10e9e4d8bcf79a996889d
최재민 <admin@chicken-moo.com>
2023-06-30 21:00:49 +0900

    update: loading spinner changed

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,17 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import styles from "&/Loading.module.css";
 
 const Loading = ({show}) => {
   return show && (
     <div className={styles.box}>
-      <div className={styles.ldsripple}><div></div><div></div></div>
-      {/* <div className={styles["sk-folding-cube"]}>
-        <div className={[styles["sk-cube1"], styles["sk-cube"]].join(" ")}></div>
-        <div className={[styles["sk-cube2"], styles["sk-cube"]].join(" ")}></div>
-        <div className={[styles["sk-cube4"], styles["sk-cube"]].join(" ")}></div>
-        <div className={[styles["sk-cube3"], styles["sk-cube"]].join(" ")}></div>
-      </div> */}
+      <span className={styles.loader}></span>
+      <div>로딩중...</div>
     </div>
   );
 };
