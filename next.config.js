b35c2446cfaa9b5f6d2edd4d2744809d82ae1823
최재민 <admin@chicken-moo.com>
2023-07-18 00:15:06 +0900

    fix: timezone

--- next.config.js
+++ next.config.js
@@ -4,7 +4,10 @@ const nextConfig = {
   publicRuntimeConfig: {
     KAKAO_REDIRECT_URI: process.env.KAKAO_REDIRECT_URI,
     KAKAO_REST_API_KEY: process.env.KAKAO_REST_API_KEY,
-  }
+  },
+  env: {
+    TZ: "Asia/Seoul", // 원하는 타임존으로 변경하세요
+  },
 };
 
 module.exports = nextConfig;
