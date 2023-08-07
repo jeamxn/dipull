a94fe498f3f28e5f36e503b685306ac60d9bd062
최재민 <admin@chicken-moo.com>
2023-08-08 08:28:25 +0900

    cron: add cron jobs

---
+++ src/pages/api/crons/washer.ts
@@ -0,0 +1,20 @@
+import moment from "moment";
+import { NextApiRequest, NextApiResponse } from "next";
+
+import { connectToDatabase } from "@/utils/db";
+
+const handler = async (
+  req: NextApiRequest, 
+  res: NextApiResponse
+) => {
+  // const date = moment()
+  // momnet time zone 
+  const date = moment().tz("Asia/Seoul");
+  if(date.hour() !== 0) return;
+
+  const client = await connectToDatabase();
+  const washCollection = await client.db().collection("wash");
+  washCollection.deleteMany({});
+};
+
+export default handler;
