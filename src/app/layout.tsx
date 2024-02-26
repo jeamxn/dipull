228b5ed0c17771c3308523f874c1ea328112b24c
최재민 <admin@chicken-moo.com>
2024-02-26 20:15:07 +0900

    chore: eslint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import React from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="kr">
       <body className={inter.className}>{children}</body>
     </html>
   );
