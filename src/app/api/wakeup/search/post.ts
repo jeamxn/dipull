bce5f7e01d6f3fa4b7ce548cd7419104ab01a3ec
최재민 <admin@chicken-moo.com>
2024-03-04 15:40:01 +0900

    feat: api time check

--- src/app/api/wakeup/search/post.ts
+++ src/app/api/wakeup/search/post.ts
@@ -1,7 +1,10 @@
+import moment from "moment";
+import "moment-timezone";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import youtubeSearch from "youtube-search";
 
+import { connectToDatabase } from "@/utils/db";
 import { verify } from "@/utils/jwt";
 
 const opts: youtubeSearch.YouTubeSearchOptions = {
@@ -26,6 +29,20 @@ const POST = async (
     headers: new_headers
   });
 
+  const client = await connectToDatabase();
+  const requestCollection = client.db().collection("request");
+  const my = await requestCollection.findOne({ id: verified.payload.id });
+  if(my?.last_search) {
+    const this_ = moment(my?.last_search || 0, "x");
+    const now = moment().tz("Asia/Seoul");
+    if(now.diff(this_, "seconds") < 15) return new NextResponse(JSON.stringify({
+      message: `검색은 15초에 한 번만 가능합니다. (${10 - now.diff(this_, "seconds")}초 남음)`,
+    }), {
+      status: 429,
+      headers: new_headers
+    });
+  }
+
   const { query } = await req.json();
   if(!query) return new NextResponse(JSON.stringify({
     message: "검색어를 입력해주세요.",
@@ -35,6 +52,13 @@ const POST = async (
   });
 
   try{
+    await requestCollection.updateOne({ id: verified.payload.id }, {
+      $set: {
+        last_search: moment().tz("Asia/Seoul").valueOf(),
+      }
+    }, {
+      upsert: true
+    });
     const search = await youtubeSearch(query, opts);
     return new NextResponse(JSON.stringify({
       message: "성공적으로 검색되었습니다.",
