a6970975cf82c1de18feb241d2f2f4bc4bb3294c
최재민 <admin@chicken-moo.com>
2024-03-19 12:44:51 +0900

    feat: comments

--- src/app/(login)/(main)/page.tsx
+++ src/app/(login)/(main)/page.tsx
@@ -2,18 +2,23 @@
 
 import React from "react";
 
+import Comments from "@/components/comments";
 import Insider from "@/provider/insider";
 
 import Iwannagohome from "./iwannagohome";
-import Meal from "./meal";
-import Timetable from "./timetable";
+// import Meal from "./meal";
+// import Timetable from "./timetable";
 
 const Home = () => {
   return (
     <Insider>
       <Iwannagohome />
-      <Meal />
-      <Timetable />
+      {/* <Timetable /> */}
+      <Comments />
+      {/* <Meal /> */}
+      <article className="w-full flex justify-center items-center">
+        <p className="text-text/40 text-sm">급식 확인은 <a className="text-primary/40 underline" href="https://디미고급식.com/" target="_blank" rel="noreffer noreferrer">디미고 급식</a>에서 확인해주세요!</p>
+      </article>
     </Insider>
   );
 };
