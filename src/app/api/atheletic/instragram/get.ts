e2c5f7a506f486057fd6cf178520a7edade84c8f
최재민 <admin@chicken-moo.com>
2024-04-05 08:29:23 +0900

    wow2

--- src/app/api/atheletic/instragram/get.ts
+++ src/app/api/atheletic/instragram/get.ts
@@ -1,3 +1,5 @@
+import moment from "moment";
+import "moment-timezone";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
@@ -24,7 +26,27 @@ const GET = async (
   });
 
   const client = await connectToDatabase();
+  const requestCollection = client.db().collection("atheletic_request");
+  const my = await requestCollection.findOne({ id: verified.payload.id });
+  if(my?.last_search) {
+    const this_ = moment(my?.last_search || 0, "x");
+    const now = moment().tz("Asia/Seoul");
+    if(now.diff(this_, "seconds") < 15) return new NextResponse(JSON.stringify({
+      message: `불러오기는 15초에 한 번만 가능합니다. (${15 - now.diff(this_, "seconds")}초 남음)`,
+    }), {
+      status: 429,
+      headers: new_headers
+    });
+  }
+
   const atheleticInstragramCollection = client.db().collection("atheletic_instragram");
+  await requestCollection.updateOne({ id: verified.payload.id }, {
+    $set: {
+      last_search: moment().tz("Asia/Seoul").valueOf(),
+    }
+  }, {
+    upsert: true
+  });
   const data = await getInstagramPosts();
   await atheleticInstragramCollection.deleteMany({});
   await atheleticInstragramCollection.insertMany(data);
--- src/app/api/atheletic/instragram/getInstagramPosts.ts
+++ src/app/api/atheletic/instragram/getInstagramPosts.ts
@@ -1,40 +1,34 @@
 import axios from "axios";
 export const getInstagramPosts = async () => {
-  const response = await axios.get("https://www.instagram.com/api/v1/tags/web_info/", {
+  const tag_name = process.env.NEXT_PUBLIC_INSTA_TAG || "";
+
+  const csrf_response = await axios.get(`https://www.instagram.com/explore/tags/${tag_name}`, {
+    headers: {
+      "Referer": "https://www.instagram.com/",
+    }
+  });
+  const csrf_token = csrf_response.headers?.["set-cookie"]?.[0].split(";")[0].split("=")[1];
+  const response = await axios.get("https://www.instagram.com/api/v1/tags/logged_out_web_info", {
     params: {
-      "tag_name": process.env.INSTA_TAG || "",
+      "tag_name": tag_name
     },
     headers: {
       "Referer": "https://www.instagram.com/explore/tags/lck/",
-      "Cookie": `ds_user_id=${process.env.INSTA_DS_USER_ID || ""}; sessionid=${process.env.INSTA_SESSIONID || ""}; `,
-      "X-IG-App-ID": process.env.INSTA_APP_ID || "",
+      "Cookie": `csrftoken=${csrf_token}`,
+      "X-IG-App-ID": "936619743392459",
     }
   });
 
-  const data = response.data.data.top.sections.map((v1: any) => {
-    const vv = v1?.layout_content;
-    let v3 = null;
-    if(vv.one_by_two_item){
-      v3 = vv.one_by_two_item.clips.items;
-    }
-    else if(vv.medias){
-      v3 = vv.medias;
-    }
-    return v3;
-  }).flat().map((v: any) => {
-    const v22 = v.media;
-    const unix_utc_time = v22.caption.created_at_utc;
-    const date = new Date(unix_utc_time * 1000);
-    const kstDate = new Date(date.getTime() + (9 * 60 * 60 * 1000));
-    const images = v22.carousel_media ? v22.carousel_media.map((v2: any) => v2.image_versions2.candidates[0].url) : [v22.image_versions2.candidates[0].url];
-  
-    return { 
-      username: v22.caption.user.username,
-      name: v22.caption.user.full_name,
-      content: v22.caption.text,
-      profileImage: v22.caption.user.hd_profile_pic_versions[0].url,
-      image: images,
-      time: kstDate,
+  const data = response.data.data.hashtag.edge_hashtag_to_media.edges.map((v: any) => {
+    const time = v.node.taken_at_timestamp;
+    const date = new Date(time * 1000);
+    return {
+      id: v.node.shortcode,
+      thumbnail: v.node.thumbnail_src,
+      time: date,
+      like: v.node.edge_liked_by.count,
+      comment: v.node.edge_media_to_comment.count,
+      text: v.node.edge_media_to_caption.edges[0]?.node.text || "",
     };
   });
 
