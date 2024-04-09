ec647722e1d3d6b5982279a1e59a23f964f6ac56
hayward2007 <hayward2007@naver.com>
2024-04-09 21:02:56 +0900

    schedule added

--- src/app/(login)/atheletic/schedule/ScheduleBox.tsx
+++ src/app/(login)/atheletic/schedule/ScheduleBox.tsx
@@ -6,16 +6,12 @@ const ScheduleBox = ({
   location,
   grading,
   participants,
-  etc,
   time,
 }:{
   event: string;
   location: string;
   grading: string;
-  participants: {
-
-  };
-  etc: string;
+  participants: string;
   time: {
     start: string;
     end: string;
@@ -38,29 +34,24 @@ const ScheduleBox = ({
     >
       <div className="flex flex-row justify-between font-semibold">
         <p>{event}</p> 
-        <p>{time.start} ~ {time.end}</p>
+        {/* <p>{time.start} ~ {time.end}</p> */}
       </div>
       {
         clicked && <>
           <p>• 위치 : {location}</p>
-          <div>
-            <p>• 배점</p>
-            {
-              grading.split("\n").map((element, i) => (
-                <p key={i} className="pl-4">{element}</p>
-              ))
-            }</div>
-          {/* <p>{participants}</p> */}
-          <p>• 기타 : {etc}</p>
+          <p>• 배점 : </p>
+          {
+            grading.split("\n").map((element, i) => (
+              <p key={i} className="pl-4"> - {element}</p>
+            ))
+          }
+          <p>• 진행 : </p>
+          {
+            participants.split("\n").map((element, i) => (
+              <p key={i} className="pl-4"> - {element}</p>
+            ))
+          }
         </>
-        // clicked && Object.entries(machine.time).map(([key, value], i) => (
-        //   <p key={i} className={[
-        //     "text-sm",
-        //     !value ? "opacity-30" : "opacity-100"
-        //   ].join(" ")}>
-        //     <b className="font-semibold">{key}</b> {value}
-        //   </p>
-        // ))
       }
     </figure>
   );
