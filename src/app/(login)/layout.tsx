5245058a2b21da0395cf3a7a1784ad57c08cdebe
최재민 <admin@chicken-moo.com>
2024-04-16 11:25:41 +0900

    feat: qrcode gen

--- src/app/(login)/layout.tsx
+++ src/app/(login)/layout.tsx
@@ -9,6 +9,7 @@ import Fast from "./(joke)/FAST";
 import FireworkFrame from "./(joke)/fireworks";
 import Logout from "./logout";
 import Menu from "./menu";
+import Qrcode from "./qrcode";
 
 const LoginedLayout = async ({
   children,
@@ -34,12 +35,15 @@ const LoginedLayout = async ({
             <article className="w-full flex justify-center items-center border-b border-text/10 px-5 py-3">
               <p className="text-primary text-lg font-semibold">디미고인 풀 서비스 V3</p>
             </article>
-            <article className="w-full py-5 px-4 border-b border-text/10 flex flex-row items-center gap-4">
-              <img src={veryfied.payload.profile_image} alt={veryfied.payload.name} width={60} height={60} className="rounded-full" />
-              <figure className="flex flex-col justify-center items-start">
-                <p className="font-semibold text-lg">{veryfied.payload.number} {veryfied.payload.name}</p>
-                <Logout />
-              </figure>
+            <article className="w-full py-4 px-4 border-b border-text/10 flex flex-row items-center justify-between gap-4">
+              <div className="flex flex-row gap-4 items-center">
+                <img src={veryfied.payload.profile_image} alt={veryfied.payload.name} width={60} height={60} className="rounded-full" />
+                <figure className="flex flex-col justify-center items-start">
+                  <p className="font-semibold text-lg">{veryfied.payload.number} {veryfied.payload.name}</p>
+                  <Logout />
+                </figure>
+              </div>
+              <Qrcode number={veryfied.payload.number} />
             </article>
             <Menu />
           </header>
