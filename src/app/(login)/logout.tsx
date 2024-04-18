729b7966e5ff2215ab4289f42c29beb7ae66a875
최재민 <admin@chicken-moo.com>
2024-04-18 11:46:05 +0900

    feat: edit button

--- src/app/(login)/logout.tsx
+++ src/app/(login)/logout.tsx
@@ -1,8 +1,8 @@
 "use client";
 
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import React from "react";
-import { toast } from "react-toastify";
 
 import instance from "@/utils/instance";
 
@@ -16,6 +16,12 @@ const Logout = () => {
 
   return (
     <div className="flex flex-row gap-1">
+      <Link 
+        className="text-sm text-text/40 hover:text-primary transition-colors"
+        href={process.env.NEXT_PUBLIC_DIMIGOIN_URI || ""}
+        target="_blank"
+      >내 정보 수정</Link>
+      <p className="text-sm text-text/40">·</p>
       <button onClick={logout} className="text-sm text-text/40 hover:text-primary transition-colors">로그아웃</button>
     </div>
   );
