1c5b0f1c4bea179cbff65dde05f7812305c84bc1
최재민 <admin@chicken-moo.com>
2024-04-21 18:48:44 +0900

    fix: title

--- src/app/(login)/bamboo/layout.tsx
+++ src/app/(login)/bamboo/layout.tsx
@@ -2,7 +2,7 @@ import { Metadata } from "next";
 import React from "react";
 
 export const metadata: Metadata = {
-  title: "자습 위치 :: 디미고인 풀 서비스 V3",
+  title: "대나무 숲 :: 디미고인 풀 서비스 V3",
 };
 
 const Layout = ({ children }: Readonly<{ children: React.ReactNode }>) => children;
