4dfe4154f3f2fb42c6f4ddaf6a5648638ddd696a
최재민 <admin@chicken-moo.com>
2024-04-21 17:52:57 +0900

    feat: bamboo

---
+++ src/app/(login)/bamboo/page.tsx
@@ -0,0 +1,164 @@
+"use client";
+
+import moment from "moment";
+import React from "react";
+
+import Insider from "@/provider/insider";
+import { alert } from "@/utils/alert";
+import instance from "@/utils/instance";
+
+const Jasup = () => {
+  const [loading, setLoading] = React.useState(false);
+  const [textarea, setTextarea] = React.useState("");
+  const [anonymous, setAnonymous] = React.useState(true);
+  const [grade, setGrade] = React.useState(true);
+  const [data, setData] = React.useState<{
+    user: string;
+    text: string;
+    timestamp: string;
+    number: number;
+  }[]>([]);
+
+  const put = async () => {
+    setLoading(true);
+    const loading_alert = alert.loading("대나무 숲에 글자 새기는 중...");
+    try{
+      const res = await instance.put("/api/bamboo", {
+        textarea, anonymous, grade,
+      });
+      await get();
+      alert.update(loading_alert, res.data.message, "success");
+      setTextarea("");
+      setAnonymous(true);
+      setGrade(true);
+    }
+    catch(e: any){
+      alert.update(loading_alert, e.response.data.message, "error");
+    }
+    setLoading(false);
+  };
+
+  const get = async () => {
+    try{
+      const res = await instance.get("/api/bamboo");
+      setData(res.data.data);
+    }
+    catch(e: any){
+      alert.warn(e.response.data.message);
+    }
+  };
+
+  React.useEffect(() => {
+    get();
+  }, []);
+
+  return (
+    <Insider>
+      <section className="flex flex-col gap-3">
+        <section className="flex flex-col gap-1">
+          <h1 className="text-xl font-semibold">디미고 대나무 숲</h1>
+          <h1 className="text-base text-[#e11d48]">문제가 될 수 있는 대나무는 검토 후 삭제됩니다.</h1>
+        </section>
+        <article className={[
+          "flex flex-col gap-1 bg-white rounded border border-text/10 p-5 justify-start items-start overflow-auto",
+          loading ? "loading_background" : "",
+        ].join(" ")}>
+          <div className="w-full h-full relative">
+            <textarea 
+              className="w-full min-h-40 h-full border border-text/10 rounded p-3 bg-transparent"
+              placeholder="제보할 내용을 입력해주세요."
+              value={textarea}
+              onChange={(e) => setTextarea(e.target.value)}
+              maxLength={380}
+            />
+            <span className="text-text/50 text-right font-light text-sm absolute right-0 bottom-0 m-4 p-2 max-w-20 rounded-sm py-1 cursor-text backdrop-blur-xl">{textarea.length}/380자</span>
+          </div>
+          <div className="flex flex-row items-center justify-end gap-2 w-full">
+            <button 
+              className={[
+                "border w-full max-w-32 text-base font-medium rounded h-10 cursor-pointer",
+                !grade ? "border-text/30 text-text/30" : "border-primary text-primary",
+              ].join(" ")}
+              onClick={() => setGrade(p => !p)}
+            >
+              학년 {grade ? "O" : "X"}
+            </button>
+            <button 
+              className={[
+                "border w-full max-w-32 text-base font-medium rounded h-10 cursor-pointer",
+                !anonymous ? "border-text/30 text-text/30" : "border-primary text-primary",
+              ].join(" ")}
+              onClick={() => setAnonymous(p => !p)}
+            >
+              익명 {anonymous ? "O" : "X"}
+            </button>
+            <button 
+              className={[
+                "border w-full max-w-32 text-base font-medium rounded h-10",
+                loading || !textarea ? "cursor-not-allowed border-text/30 text-text/30" : "cursor-pointer bg-primary text-white",
+              ].join(" ")}
+              onClick={put}
+            >
+              제보하기
+            </button>
+          </div>
+        </article>
+      </section>
+      <section className="flex flex-col gap-3">
+        <section className="flex flex-col gap-1">
+          <h1 className="text-xl font-semibold">제보 목록</h1>
+        </section>
+        {
+          data.length ? data.map((item, index) => {
+            const diff = moment().diff(moment(item.timestamp, "YYYY-MM-DD HH:mm:ss"), "minutes");
+            return (
+              <article 
+                key={index}
+                className={[
+                  "flex flex-col gap-1 bg-white rounded border border-text/10 p-5 justify-start items-start overflow-auto",
+                  loading ? "loading_background" : "",
+                ].join(" ")}
+                id={`${index}`}
+              >
+                <div className="flex flex-row items-center justify-between w-full">
+                  <div className="flex flex-row">
+                    <b className="font-medium">{item.user}</b>의 대나무&nbsp;
+                  </div>
+                  <p className="text-text/30">
+                    {
+                      diff < 1 ? "방금 전" :
+                        diff < 60 ? `${diff}분 전` :
+                          diff < 1440 ? `${Math.floor(diff / 60)}시간 전` :
+                            diff < 10080 ? `${Math.floor(diff / 1440)}일 전` :
+                              diff < 40320 ? `${Math.floor(diff / 10080)}주 전` :
+                                diff < 525600 ? `${Math.floor(diff / 40320)}달 전` :
+                                  `${Math.floor(diff / 525600)}년 전`
+                    }
+                    &nbsp;(#{item.number || 0})
+                  </p>
+                </div>
+                <div>
+                  {
+                    item.text.split("\n").map((line, i) => (
+                      <p key={i}>{line}</p>
+                    ))
+                  }
+                </div>
+              </article>
+            );
+          }) : (
+            <article className={[
+              "flex flex-col gap-1 bg-white rounded border border-text/10 p-5 justify-start items-center overflow-auto",
+              loading ? "loading_background" : "",
+            ].join(" ")}>
+              <p className="text-text/40">제보된 대나무가 없습니다.</p>
+            </article>
+          )
+        }
+      </section>
+    </Insider>
+  );
+};
+
+
+export default Jasup;
---
+++ src/app/api/bamboo/put.ts
@@ -0,0 +1,76 @@
+import "moment-timezone";
+import moment from "moment";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+
+import { connectToDatabase } from "@/utils/db";
+import { verify } from "@/utils/jwt";
+
+const PUT = async (
+  req: Request,
+) => {
+  // 헤더 설정
+  const new_headers = new Headers();
+  new_headers.append("Content-Type", "application/json; charset=utf-8");
+  
+  // Authorization 헤더 확인
+  const authorization = headers().get("authorization");
+  const verified = await verify(authorization?.split(" ")[1] || "");
+  if(!verified.ok || !verified.payload?.id) return new NextResponse(JSON.stringify({
+    message: "로그인이 필요합니다.",
+  }), {
+    status: 401,
+    headers: new_headers
+  });
+
+  const { textarea, anonymous, grade } = await req.json();
+  if(!textarea) return new NextResponse(JSON.stringify({
+    message: "내용을 입력해주세요.",
+  }), {
+    status: 400,
+    headers: new_headers
+  });
+  else if(textarea.length > 380) return new NextResponse(JSON.stringify({
+    message: "최대 380자까지 입력 가능합니다.",
+  }), {
+    status: 400,
+    headers: new_headers
+  });
+  
+  const client = await connectToDatabase();
+  const statesCollection = client.db().collection("states");
+  const state = await statesCollection.findOneAndUpdate({
+    type: "bamboo",
+  }, {
+    $inc: {
+      count: 1,
+    }
+  }, {
+    upsert: true,
+    returnDocument: "after",
+  });
+  const bambooCollection = client.db().collection("bamboo");
+  const { insertedId } = await bambooCollection.insertOne({
+    user: verified.payload.id,
+    text: textarea,
+    timestamp: moment().tz("Asia/Seoul").format("YYYY-MM-DD HH:mm:ss"),
+    anonymous: anonymous, 
+    grade: grade,
+    number: state?.count,
+  });
+
+  if(!insertedId) return new NextResponse(JSON.stringify({
+    message: "제보에 실패했습니다.",
+  }), {
+    status: 500,
+    headers: new_headers
+  });
+  else return new NextResponse(JSON.stringify({
+    message: "제보에 성공했습니다.",
+  }), {
+    status: 200,
+    headers: new_headers
+  });  
+};
+
+export default PUT;
