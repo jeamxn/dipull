3bf7272f1b34169f2171f1e15ec9b0c037e363d1
Jeamin Choi <admin@chicken-moo.com>
2024-04-26 09:54:49 +0900

    fix: imports (#129)

--- src/app/(login)/jasup/layout.tsx
+++ src/app/(login)/jasup/layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import SubMenu from "@/components/submenu";
 import Insider from "@/provider/insider";
 
-import { menu } from "./route";
+import { menu } from "./utils";
 
 const Layout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
   return (
