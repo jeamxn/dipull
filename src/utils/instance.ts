0911235270319590b34fd3fefbff7ead3d93f8d1
Jeamin Choi <admin@chicken-moo.com>
2024-05-10 22:09:21 +0900

    fix: bug (#166)

--- src/utils/instance.ts
+++ src/utils/instance.ts
@@ -22,12 +22,27 @@ instance.interceptors.response.use(
   (response) => response, 
   async (error) => {
     if(error.response.status === 401) {
-      const refresh = await axios.get("/auth/refresh");
-      const accessToken = refresh.data.accessToken;
-      localStorage.setItem("accessToken", accessToken);
-      error.config.headers.Authorization = `Bearer ${accessToken}`;
-      const originalResponse = await axios.request(error.config);
-      return originalResponse.data.data;
+      try{
+        const refresh = await axios.get("/auth/refresh");
+        const accessToken = refresh.data.accessToken;
+        localStorage.setItem("accessToken", accessToken);
+        error.config.headers.Authorization = `Bearer ${accessToken}`;
+        const originalResponse = await axios.request(error.config);
+        return originalResponse.data.data;
+      }
+      catch (e: any) {
+        if(e.response.status === 401) {
+          await axios.get("/auth/logout", {
+            withCredentials: true,
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("accessToken")}`
+            }
+          });
+          localStorage.removeItem("accessToken");
+          localStorage.removeItem("refreshToken");
+          window.location.href = "/login";
+        }
+      }
     }
     return Promise.reject(error);
   }
