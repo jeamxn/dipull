a904602f2b5923ba9c18cba93c682a261c5b0719
jeamxn <admin@chicken-moo.com>
2024-06-19 08:49:59 +0900

    write: url change

--- src/app/(login)/(header)/footer.tsx
+++ src/app/(login)/(header)/footer.tsx
@@ -15,7 +15,8 @@ const Footer = () => {
         <Fast />
         <Alert />
         <p className="text-text/40 text-sm text-center">급식 확인은 <a className="text-primary/40 underline" href="https://디미고급식.com/" target="_blank" rel="noreferrer">디미고 급식</a>에서 확인해주세요!</p>
-        <p className="text-text/40 text-sm text-center">서비스 사용 방법은 <a className="text-primary/40 underline" href="https://docs.dimigo.net" target="_blank" rel="noreferrer">여기</a>를 참고 해주세요!</p>
+        <p className="text-text/40 text-sm text-center">서비스 사용 방법은 <a className="text-primary/40 underline" href="https://docs.dimigo.net" target="_blank" rel="noreferrer">사용 설명서</a>를 참고 해주세요!</p>
+        <p className="text-text/40 text-sm">오류 및 기타 문의 사항은 <a className="text-primary/40 underline" href="https://github.com/jeamxn/dipull" target="_blank" rel="noreferrer">디풀 개발자 커뮤니티</a>에 연락바랍니다!</p>
       </article>
     </footer>
   ) : null;
