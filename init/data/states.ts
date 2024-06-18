01391300ef78b540b3ae6aff485a3062289de33c
Jeamin Choi <admin@chicken-moo.com>
2024-06-18 09:49:34 +0900

    feat: init fc (#193)

---
+++ init/data/states.ts
@@ -0,0 +1,47 @@
+const states = [
+  {
+    "type": "machine",
+    "data": {
+      "start": "",
+      "end": ""
+    }
+  },
+  {
+    "type": "stay",
+    "data": {
+      "start": "",
+      "end": "",
+      "grade12Subtract": 0,
+      "grade3Add": 1
+    }
+  },
+  {
+    "type": "atheletic_current_event",
+    "data": ""
+  },
+  {
+    "type": "atheletic_teams_name",
+    "data": {
+      "left": "선생님",
+      "right": "학생회"
+    }
+  },
+  {
+    "type": "bamboo",
+    "count": 0
+  },
+  {
+    "type": "bamboo_comment",
+    "count": {}
+  },
+  {
+    "type": "class_stay",
+    "data": {
+      "1": false,
+      "2": false,
+      "3": false
+    }
+  }
+];
+
+export default states;
