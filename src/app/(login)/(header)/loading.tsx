cde7e0682e11c5a9ec33fc8bb779853e70f462cf
Jeamin Choi <admin@chicken-moo.com>
2024-06-20 10:36:40 +0900

    edit: loading bar design (#203)

--- src/app/(login)/(header)/loading.tsx
+++ src/app/(login)/(header)/loading.tsx
@@ -10,22 +10,12 @@ const Loading = ({
   fixed?: boolean,
 }) => {
   const [loading, setLoading] = useRecoilState(loadingAtom);
-  const [end, setEnd] = React.useState(true);
-
-  React.useEffect(() => { 
-    if (loading) {
-      setEnd(false);
-    }
-  }, [loading]);
 
   const pathname = usePathname();
   const handleComplete = () => {
-    // if (!loading) return;
-    setEnd(true);
     setTimeout(() => { 
       setLoading(false);
-      setEnd(false);
-    }, 200);
+    }, 500);
   };
   React.useEffect(() => {
     window.addEventListener("load", handleComplete);
@@ -39,13 +29,13 @@ const Loading = ({
 
   return (
     <div className={[
-      "w-full",
-      fixed ? "fixed top-0 z-50" : "absolute bottom-0"
+      "w-full h-[0.125rem]",
+      fixed ? "fixed top-0 z-50" : "absolute bottom-0",
     ].join (" ")}>
       <div
         className={[
-          "border-primary border-b-2 ease-in-out transition-all",
-          end ? "w-full duration-200" : loading ? "w-10/12 duration-[5s]" : "w-0 transition-none",
+          "h-full rainbow ease-in-out transition-all",
+          loading ? "w-full duration-500" : "w-0 transition-none",
         ].join(" ")}
       />
     </div>
