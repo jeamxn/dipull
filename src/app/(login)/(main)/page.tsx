bcb57802b2dc87a953136b70dcea22cd09dc03c6
jeamxn <admin@chicken-moo.com>
2024-06-23 20:27:57 +0900

    rm: update log

--- src/app/(login)/(main)/page.tsx
+++ src/app/(login)/(main)/page.tsx
@@ -20,19 +20,6 @@ const Home = async () => {
   return (
     <>
       <Insider>
-        {
-          userInfo.type === "student" ? (
-            <article className="flex flex-col gap-3">
-              <section className="flex flex-col gap-1">
-                <h1 className="text-xl font-semibold">업데이트 내역</h1>
-                <div className="flex flex-col gap-1">
-                  <p className="text-base text-primary">1. SSR(서버 사이드 랜더링) 기술을 도입했습니다!</p>
-                  <p className="text-base text-primary">2. 로딩 프로세스 바를 추가했습니다.</p>
-                </div>
-              </section>
-            </article>
-          ) : null
-        }
         {/* {
           userInfo.number > 3000 && userInfo.gender === "male" ? (
             <HosilInfo />
