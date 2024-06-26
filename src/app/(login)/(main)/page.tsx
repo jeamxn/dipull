241ff8a8937482fdf0886b3aad97a13fa0fdffdd
jeamxn <admin@chicken-moo.com>
2024-06-27 08:45:28 +0900

    feat: meal

--- src/app/(login)/(main)/page.tsx
+++ src/app/(login)/(main)/page.tsx
@@ -1,7 +1,10 @@
+import "moment-timezone";
+import moment from "moment";
 import { cookies } from "next/headers";
 import React from "react";
 
 import { getIwannagohome } from "@/app/api/iwannagohome/server";
+import { getMeal } from "@/app/api/meal/[date]/server";
 import { getTimetable } from "@/app/api/timetable/[grade]/[class]/server";
 import { defaultUserData } from "@/app/auth/type";
 import Insider from "@/provider/insider";
@@ -10,6 +13,7 @@ import { verify } from "@/utils/jwt";
 import HosilInfo from "./hosilInfo";
 import Iwannagohome from "./iwannagohome";
 import Luck from "./luck";
+import Meal from "./meal";
 import Timetable from "./timetable";
 
 const Home = async () => {
@@ -20,6 +24,8 @@ const Home = async () => {
   const iwannagoHomeInit = await getIwannagohome(userInfo.id);
   const gradeClass = Math.floor(userInfo.number / 100);
   const timetalbeInit = await getTimetable(Math.floor(gradeClass / 10), gradeClass % 10);
+  const today = moment().tz("Asia/Seoul").format("YYYY-MM-DD");
+  const mealInit = await getMeal(today);
 
   return (
     <>
@@ -30,8 +36,6 @@ const Home = async () => {
               <section className="flex flex-col gap-1">
                 <h1 className="text-xl font-semibold">알림 기능 추가</h1>
                 <div className="flex flex-col gap-1">
-                  <p className="text-base text-primary">세탁기, 건조기 사용 전후로 알림을 전송합니다.</p>
-                  <p className="text-base text-primary">잔류 신청 마감 알림을 전송합니다.</p>
                   <p className="text-base text-primary">메뉴의 종모양을 클릭하여 알림 권한을 허용해주세요!</p>
                   <p className="text-base text-primary">핸드폰 설정에서 사용하는 브라우저의 알림 또한 허용해주세요.</p>
                 </div>
@@ -48,6 +52,7 @@ const Home = async () => {
           init={timetalbeInit}
           userInfo={userInfo}
         />
+        <Meal init={mealInit} />
         <Luck />
         <Iwannagohome
           init={{
