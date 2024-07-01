406f170bdeef21e4eadcdfe083f041f457c0832a
yeonfish6040 <yeonfish6040@gmail.com>
2024-07-01 12:57:15 +0900

    fix: code structure

--- src/app/api/wakeup/selected/get.ts
+++ src/app/api/wakeup/selected/get.ts
@@ -1,7 +1,7 @@
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
-import { connectToDatabase } from "@/utils/db";
+import { getStates } from "@/utils/getStates";
 import { verify } from "@/utils/jwt";
 
 const GET = async (
@@ -21,14 +21,10 @@ const GET = async (
     headers: new_headers
   });
 
-  const client = await connectToDatabase();
-  const statesCollection = client.db().collection("states");
-  const data = await statesCollection.findOne({
-    type: "wakeup_selected"
-  });
 
+  const data = await getStates("wakeup_selected");
   return new NextResponse(JSON.stringify({
-    data: data?.data[verified.payload.data.gender],
+    data: data[verified.payload.data.gender],
   }), {
     status: 200,
     headers: new_headers
