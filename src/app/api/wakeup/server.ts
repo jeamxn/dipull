7b335f379001f01731b0bd20bf3ed2c6e8f4022c
yeonfish6040 <yeonfish6040@gmail.com>
2024-07-04 08:39:08 +0900

    fix: strict checking

--- src/app/api/wakeup/server.ts
+++ src/app/api/wakeup/server.ts
@@ -70,7 +70,7 @@ export const getSelected = async (gender: string) => {
     type: "wakeup_selected"
   }))?.data[gender];
 
-  if (Object.prototype.hasOwnProperty.call(data, "date") || data.date === undefined || data.date === "") return { id: null };
+  if (data || data.date) return { id: null };
 
   return {
     ...data,
