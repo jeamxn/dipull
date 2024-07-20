d5311bcee8733b532fdd367f76f6086e69dcae68
Jeamin Choi <admin@chicken-moo.com>
2024-07-20 16:18:45 +0900

    feat: toLocaleString (#329)

--- src/app/(login)/wakeup/my/MyAvailContent.tsx
+++ src/app/(login)/wakeup/my/MyAvailContent.tsx
@@ -44,7 +44,7 @@ const MyAvailContent = ({
     <article className="flex flex-col gap-3">
       <section className="flex flex-col gap-1">
         <h1 className="text-xl font-semibold">나의 신청 가능 횟수</h1>
-        <h1 className="text-base text-primary">홀/짝 맞추면 신청권 2배, 틀리면 0개. 1주일에 기본 신청권 {defaultWakeupAvail}개.</h1>
+        <h1 className="text-base text-primary">홀/짝 맞추면 신청권 2배, 틀리면 0개. 1주일에 기본 신청권 {defaultWakeupAvail.toLocaleString()}개.</h1>
       </section>
       <div className={[
         "flex flex-row items-center justify-between bg-white border border-text/10 py-4",
@@ -57,7 +57,7 @@ const MyAvailContent = ({
         <div className="w-full flex flex-col items-center justify-center">
           <div className="flex flex-row items-center justify-center gap-[0.325rem] w-full">
             <p className="font-light text-sm">신청권</p>
-            <p className="font-semibold text-xl">{avail}개</p>
+            <p className="font-semibold text-xl">{avail.toLocaleString()}개</p>
           </div>
           <div className="flex flex-row items-center justify-center gap-[0.325rem] w-full">
             <p className="font-light text-sm">나온 숫자</p>
