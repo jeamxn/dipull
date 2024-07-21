cd3b99a1707017fcd61eddf4252717abf36b0cdf
jeamxn <admin@chicken-moo.com>
2024-07-21 22:03:04 +0900

    fix: bug

--- src/app/(login)/wakeup/my/page.tsx
+++ src/app/(login)/wakeup/my/page.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { getWakeupAvail } from "@/app/api/wakeup/apply/server";
 import { getWakeupRanking } from "@/app/api/wakeup/ranking/server";
 import { getWakeup } from "@/app/api/wakeup/server";
+import { getStates } from "@/utils/getStates";
 import { getUserInfo } from "@/utils/server";
 
 import Content from "./content";
@@ -12,11 +13,14 @@ const MachinePage = async () => {
   const initialData = await getWakeup(initialUserInfo.id, initialUserInfo.gender);
   const initailAvail = await getWakeupAvail(initialUserInfo.id);
   const initailRanking = await getWakeupRanking();
+  const emergency = await getStates("emergency");
+  
   return (
     <Content
       myAvail={initailAvail.available}
       initailData={initialData}
       initailRanking={initailRanking}
+      emergency={emergency}
     />
   );
 };
