4f20d9ad2266f49b50aef11698489469a7927a9a
jeamxn <admin@chicken-moo.com>
2024-07-22 13:54:23 +0900

    feat: navigation move

--- src/components/Navigation/button.tsx
+++ src/components/Navigation/button.tsx
@@ -7,30 +7,43 @@ const Button = ({
   icon,
   text,
   url,
+  setWidth,
 }: {
   icon: React.ReactNode;
   text: string;
   url: string;
+  setWidth: React.Dispatch<React.SetStateAction<number>>;
 }) => {
   const path = usePathname();
+  const isSame = path === url;
+  const ref = React.useRef<HTMLDivElement>(null);
+
+  React.useEffect(() => {
+    if (ref.current && isSame) {
+      setWidth(ref.current.offsetWidth);
+    }
+  }, [ref.current, isSame]);
+
   return (
-    <Linker href={url} className="py-2 flex flex-row items-center justify-center transition-all">
-      {
-        path === url ? (
-          <div className="bg-white rounded-full p-4 flex flex-row items-center justify-start gap-3">
-            <svg className="fill-text w-6 h-6" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-              {icon}
-            </svg>
-            <p className="font-semibold text-base">{text}</p>
-          </div>
-        ) : (
-          <div className="rounded-full p-4 flex flex-row items-center justify-start gap-3">
-            <svg className="fill-white w-6 h-6" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-              {icon}
-            </svg>
-          </div>
-        )
-      }
+    <Linker href={url} className="py-2 flex flex-row items-center justify-center transition-all z-50">
+      <div
+        ref={ref}
+        className={[
+          "rounded-full p-4 flex flex-row items-center justify-start gap-3",
+        // isSame ? "bg-white" : ""
+        ].join(" ")}
+      >
+        <svg className={[
+          "w-6 h-6 transition-all",
+          isSame ? "fill-text" : "fill-white",
+        ].join(" ")} viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+          {icon}
+        </svg>
+        <p className={[
+          "font-semibold text-base",
+          isSame ? "text-text block" : "text-white hidden",
+        ].join(" ")}>{text}</p>
+      </div>
     </Linker>
   );
 };
--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -9,8 +9,22 @@ import { Links } from "./links";
 const Navigation = () => { 
   const path = usePathname();
   const show = Links.some((link) => link.url === path);
+  const findIndex = Links.findIndex((link) => link.url === path);
+  const [width, setWidth] = React.useState(0);
+
   return show ? (
     <div className="absolute bottom-8 w-full flex flex-row items-center justify-center">
+      <div
+        className={[
+          "h-full transition-all absolute py-2",
+        ].join(" ")}
+        style={{
+          width,
+          transform: `translateX(${-112 + 56 * findIndex}px)`,
+        }}
+      >
+        <div className="w-full h-full bg-white rounded-full" />
+      </div>
       <div className="bg-text/80 rounded-full flex flex-row gap-0 items-center justify-center px-2">
         {
           Links.map((link) => (
@@ -19,6 +33,7 @@ const Navigation = () => {
               icon={link.icon}
               text={link.text}
               url={link.url}
+              setWidth={setWidth}
             />
           ))
         }
