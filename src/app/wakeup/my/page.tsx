86b598f94b0d7a721a1c4f2aebd879a190845573
jeamxn <admin@chicken-moo.com>
2024-07-23 22:48:44 +0900

    feat: wakeup

---
+++ src/app/wakeup/my/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from "react";
+
+
+const Machine = () => {
+  return (
+    <div className="flex flex-col gap-4 w-full">
+      
+    </div>
+  );
+};
+
+export default Machine;
