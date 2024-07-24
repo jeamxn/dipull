2168b68c40ecb160d58f87f38760ffa6d92e2fa5
jeamxn <admin@chicken-moo.com>
2024-07-24 14:32:36 +0900

    feat: separate

---
+++ src/app/machine/[type]/apply.tsx
@@ -0,0 +1,87 @@
+import moment from "moment";
+import React from "react";
+
+import * as Select from "@/components/Select";
+import { useAuth, useUserInfo } from "@/hooks";
+import { Machine as MachineInfo, Machine_list, MachineJoin } from "@/utils/db/utils";
+
+import { MachineType, machineTypeToKorean } from "./utils";
+
+const Apply = ({
+  params,
+  machines,
+  machinesLoading,
+  machine_current,
+  machine_currentLoading,
+  times,
+}: {
+    params: { type: MachineType },
+    machines: Machine_list[] | undefined,
+    machinesLoading: boolean,
+    machine_current: MachineJoin[] | undefined,
+    machine_currentLoading: boolean,
+    times: MachineInfo["time"][],
+  }) => {
+  const user = useUserInfo();
+  const { needLogin } = useAuth();
+  const current_korean = machineTypeToKorean(params.type);
+  const [machine, setMachine] = React.useState<MachineInfo["code"]>();
+  const [time, setTime] = React.useState<MachineInfo["time"]>();
+
+  const time_disables = React.useMemo(() => { 
+    const mapped = machine_current?.map((m) => {
+      if (m.code === machine) {
+        return m.time;
+      }
+      else {
+        return null;
+      }
+    });
+    const filtered = mapped?.filter((m) => m);
+    const timesMaped = times.map((time) => { 
+      if (filtered?.includes(time)) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    });
+    return timesMaped || [];
+  }, [machine_current, machine]);
+
+  return (
+    <>
+      <Select.Full
+        label={`${current_korean}기 선택`}
+        placeholder={`${current_korean}기를 선택해주세요.`}
+        options={machines?.map((m) => m.name)}
+        optionValues={machines?.map((m) => m.code)}
+        value={machine}
+        onConfirm={(value) => setMachine(value)}
+        disabled={machinesLoading}
+      />
+      <Select.Full
+        label={`${current_korean} 시간 선택`}
+        placeholder={`${current_korean} 시간을 선택해주세요.`}
+        options={times.map((time) => moment(time, "HH:mm").format("a h시 mm분"))}
+        optionValues={times}
+        value={time}
+        onConfirm={(value) => setTime(value)}
+        disables={time_disables}
+        disabled={!machine || machine_currentLoading}
+      />
+      <div className="px-6 w-full">
+        <button
+          className={[
+            "p-3 bg-text dark:bg-text-dark text-white dark:text-white-dark rounded-xl font-semibold w-full transition-all",
+            machine && time ? "cursor-pointer" : "cursor-not-allowed opacity-50",
+          ].join(" ")}
+          disabled={!machine || !time}
+          onClick={user.id ? () => { } : needLogin}
+        >신청하기</button>
+      </div>
+    </>
+  );
+};
+
+export default Apply;
--- src/app/machine/[type]/page.tsx
+++ src/app/machine/[type]/page.tsx
@@ -6,18 +6,15 @@ import moment from "moment";
 import React from "react";
 
 import Linker from "@/components/Linker";
-import * as Select from "@/components/Select";
-import { useAuth, useUserInfo } from "@/hooks";
-import { Machine as MachinType, Machine_list, MachineJoin } from "@/utils/db/utils";
+import { useUserInfo } from "@/hooks";
+import { Machine_list, MachineJoin } from "@/utils/db/utils";
 
+import Apply from "./apply";
 import { MachineType, machineTypeToKorean } from "./utils";
 
 const Machine = ({ params }: { params: { type: MachineType } }) => {
   const user = useUserInfo();
-  const { needLogin } = useAuth();
   const current_korean = machineTypeToKorean(params.type);
-  const [machine, setMachine] = React.useState<MachinType["code"]>();
-  const [time, setTime] = React.useState<MachinType["time"]>();
 
   const { data: machines, isLoading: machinesLoading } = useQuery({
     queryKey: ["machine_list", { type: params.type }],
@@ -44,27 +41,6 @@ const Machine = ({ params }: { params: { type: MachineType } }) => {
     "22:30",
   ];
 
-  const time_disables = React.useMemo(() => { 
-    const mapped = machine_current?.map((m) => {
-      if (m.code === machine) {
-        return m.time;
-      }
-      else {
-        return null;
-      }
-    });
-    const filtered = mapped?.filter((m) => m);
-    const timesMaped = times.map((time) => { 
-      if (filtered?.includes(time)) {
-        return true;
-      }
-      else {
-        return false;
-      }
-    });
-    return timesMaped || [];
-  }, [machine_current, machine]);
-
   return (
     <div className="w-full py-6 flex flex-col gap-6">
       <div className="flex flex-row items-center justify-center gap-1 px-6 w-full">
@@ -85,36 +61,16 @@ const Machine = ({ params }: { params: { type: MachineType } }) => {
 
       <div className="flex flex-col gap-4">
         <p className="text-lg font-semibold px-6 text-text dark:text-text-dark">{current_korean}기 신청하기</p>
-        <Select.Full
-          label={`${current_korean}기 선택`}
-          placeholder={`${current_korean}기를 선택해주세요.`}
-          options={machines?.map((m) => m.name)}
-          optionValues={machines?.map((m) => m.code)}
-          value={machine}
-          onConfirm={(value) => setMachine(value)}
-          disabled={machinesLoading}
+        <Apply
+          params={params}
+          machines={machines}
+          machinesLoading={machinesLoading}
+          machine_current={machine_current}
+          machine_currentLoading={machine_currentLoading}
+          times={times}
         />
-        <Select.Full
-          label={`${current_korean} 시간 선택`}
-          placeholder={`${current_korean} 시간을 선택해주세요.`}
-          options={times.map((time) => moment(time, "HH:mm").format("a h시 mm분"))}
-          optionValues={times}
-          value={time}
-          onConfirm={(value) => setTime(value)}
-          disables={time_disables}
-          disabled={!machine || machine_currentLoading}
-        />
-        <div className="px-6 w-full">
-          <button
-            className={[
-              "p-3 bg-text dark:bg-text-dark text-white dark:text-white-dark rounded-xl font-semibold w-full transition-all",
-              machine && time ? "cursor-pointer" : "cursor-not-allowed opacity-50",
-            ].join(" ")}
-            disabled={!machine || !time}
-            onClick={user.id ? () => { } : needLogin}
-          >신청하기</button>
-        </div>
       </div>
+
       <div className="flex flex-col gap-4">
         <p className="text-lg font-semibold px-6 text-text dark:text-text-dark">{current_korean} 신청 현황</p>
         <div className="px-6 overflow-x-auto overflow-y-hidden snap-x snap-mandatory">
